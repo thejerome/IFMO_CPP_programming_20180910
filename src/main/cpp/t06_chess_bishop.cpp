824d0a7c861756c6341da8d7561e2d7765111f6b
belonel <anatot92>
2018-10-10 00:33:05 +0300

    t06 done

--- src/main/cpp/t06_chess_bishop.cpp
+++ src/main/cpp/t06_chess_bishop.cpp
@@ -16,9 +16,17 @@
 
 #include "t06_chess_bishop.h"
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int t06_chess_bishop() {
+    int x1, y1, x2, y2;
+    cin>>x1>>y1>>x2>>y2;
+
+    if (abs(x2 - x1) == abs(y2 - y1))
+        cout<<"YES";
+    else
+        cout<<"NO";
 
 };
