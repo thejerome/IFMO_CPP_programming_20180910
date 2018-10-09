109608c98e2f4dc7eee8fbc46631801c443df067
belonel <anatot92>
2018-10-10 00:04:22 +0300

    t05 solved with using abs()

--- src/main/cpp/t05_chess_king.cpp
+++ src/main/cpp/t05_chess_king.cpp
@@ -16,9 +16,17 @@
 
 #include "t05_chess_king.h"
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int t05_chess_king() {
+    int x1, y1, x2, y2;
+    cin>>x1>>y1>>x2>>y2;
+
+    if (abs(x2 - x1) <= 1 && abs(y2 - y1) <= 1)
+        cout<<"YES";
+    else
+        cout<<"NO";
 
 };
