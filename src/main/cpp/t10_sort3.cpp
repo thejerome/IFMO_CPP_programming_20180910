b8e1bb08c9c2ef739b1164989783552591bb1cbb
belonel <anatot92>
2018-10-10 22:31:54 +0300

    t10 done

--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -19,5 +19,31 @@
 using namespace std;
 
 int t10_sort3() {
+    int a, b, c;
+    cin>>a>>b>>c;
 
+    if (a <= b)
+    {
+        if (a <= c) {
+            cout<<a<<' ';
+            if (b <= c)
+                cout<<b<<' '<<c;
+            else
+                cout<<c<<' '<<b;
+        }
+        else
+            cout<<c<<' '<<a<<' '<<b;
+    }
+    else
+    {
+        if (b <= c) {
+            cout<<b<<' ';
+            if (a <= c)
+                cout<<a<<' '<<c;
+            else
+                cout<<c<<' '<<a;
+        }
+        else
+            cout<<c<<' '<<b<<' '<<a;
+    }
 };
