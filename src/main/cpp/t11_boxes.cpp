b7a871a53de2f28dd3ef48e3cd9607fabb1fae0e
belonel <anatot92>
2018-10-11 11:26:11 +0300

    All done

--- src/main/cpp/t11_boxes.cpp
+++ src/main/cpp/t11_boxes.cpp
@@ -39,7 +39,6 @@ using namespace std;
 int t11_boxes() {
     int a1, b1, c1, a2, b2, c2;
     cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
-
     if ((a1 == a2 && b1 == b2 && c1 == c2) ||
         (a1 == b2 && b1 == a2 && c1 == c2) ||
         (a1 == c2 && b1 == b2 && c1 == a2) ||
