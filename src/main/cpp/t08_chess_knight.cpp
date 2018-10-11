c8e04b785eb7d71fc3e9ef17a1df2870d2b244e2
Vgolovanov2000 <supersherman32@gmail.com>
2018-10-11 18:05:09 +0300

    task-2 done

--- src/main/cpp/t08_chess_knight.cpp
+++ src/main/cpp/t08_chess_knight.cpp
@@ -29,5 +29,12 @@
 using namespace std;
 
 int t08_chess_knight() {
-
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    if ((abs(x1 - x2) == 1 && abs(y1 - y2) == 2 )|| (abs (y1-y2) == 1 && abs (x1 - x2) == 2)) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
 };
