634aaba130faa9c30ab8e095edacff6385e3813e
Антон Милорадов <usr.tt@yandex.ru>
2018-10-14 19:33:22 +0300

    first tasks

--- src/main/cpp/t01_max3.cpp
+++ src/main/cpp/t01_max3.cpp
@@ -18,5 +18,21 @@
 using namespace std;
 
 int t01_max3() {
+int a,b,c;
 
+cin >> a >> b >> c;
+
+if (a > b) {
+    if (a > c) {
+        cout << a;
+    } else {
+        cout << c;
+    }
+} else {
+    if (b > c) {
+        cout << b;
+    } else {
+        cout << c;
+    }
+}
 };
