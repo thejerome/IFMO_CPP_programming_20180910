11d0fb92a3c3ec0dc937b13745fa104ae351ab2d
amandany <dan.aman@yandex.ru>
2018-10-15 22:11:11 +0300

    Solved all task - 2 with hard work

--- src/main/cpp/t01_max3.cpp
+++ src/main/cpp/t01_max3.cpp
@@ -18,5 +18,23 @@
 using namespace std;
 
 int t01_max3() {
+    int a, b , c ;
+bool d;
+d=false;
+    cin >> a >> b >> c ;
+    if (d==false){
+    if (a>=b && a>=c) {
+        cout << a <<endl;
+        d=true;
+    }}
 
+    if (d==false){
+    if (b>=a && b>=c) {
+        cout << b <<endl;
+        d=true;
+    }}
+    if (d==false) {
+        if (c >= b && c >= a) {
+            cout << c << endl;
+        }}
 };
