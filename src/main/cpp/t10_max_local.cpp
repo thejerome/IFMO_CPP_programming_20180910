fbb1697b1f070753a5dc6d88408fd3605bdfa981
Антон Милорадов <usr.tt@yandex.ru>
2018-10-15 21:00:34 +0300

    t010 (task-3)

--- src/main/cpp/t10_max_local.cpp
+++ src/main/cpp/t10_max_local.cpp
@@ -25,4 +25,25 @@ using namespace std;
 
 int t10_max_local() {
 
+    int prv, cur, nxt;
+    int count = 0;
+    if (cin>>prv && prv)
+    {
+        if (cin >> cur && cur)
+        {
+            if (cin >> nxt)
+            {
+                while (nxt)
+                {
+                    if ((prv < cur) && (cur > nxt))
+                        count++;
+                    prv = cur;
+                    cur = nxt;
+                    cin >> nxt;
+                }
+            }
+        }
+    }
+    cout << count;
+
 };
