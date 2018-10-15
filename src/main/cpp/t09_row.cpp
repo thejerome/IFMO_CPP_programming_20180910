b759bed88b242a88d1e5d504332cb3b4d1748333
Антон Милорадов <usr.tt@yandex.ru>
2018-10-15 20:59:37 +0300

    t09 (task-3)

--- src/main/cpp/t09_row.cpp
+++ src/main/cpp/t09_row.cpp
@@ -24,4 +24,21 @@ using namespace std;
 
 int t09_row() {
 
+    int cur;
+    cin >> cur;
+    int base = cur, count = 0, count_max = 0;
+    while (cur)
+    {
+        if (base == cur)
+            count++;
+        else
+        {
+            base = cur;
+            count = 1;
+        }
+        count_max = max(count_max, count);
+        cin >> cur;
+    }
+    cout << count_max;
+
 };
