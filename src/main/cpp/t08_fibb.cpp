bc6461585362e0aa6bca6d61d3f5822a16e6b13b
Антон Милорадов <usr.tt@yandex.ru>
2018-10-15 20:57:22 +0300

    t08 (task-3)

--- src/main/cpp/t08_fibb.cpp
+++ src/main/cpp/t08_fibb.cpp
@@ -22,4 +22,19 @@ using namespace std;
 
 int t08_fibb() {
 
+    int n, i = 2, f1 = 0, f2 = 1, cur;
+    cin >> n;
+
+    while (i <= n)
+    {
+        cur = f1 + f2;
+        f1 = f2;
+        f2 = cur;
+        i++;
+    }
+    if (n<=1)
+        cout<<n;
+    else
+        cout << cur;
+
 };
