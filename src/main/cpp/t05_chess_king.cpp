951d3b09d0888b750035ef030e2dfe7b4adc2604
Budurgus <burdaev-igor@mail.ru>
2018-10-16 21:00:30 +0300

    Task 2

--- src/main/cpp/t05_chess_king.cpp
+++ src/main/cpp/t05_chess_king.cpp
@@ -22,7 +22,7 @@ using namespace std;
 int t05_chess_king() {
 int x, y, X, Y;
 cin >> x >> y >> X >> Y;
-    if (((abs(x - X) == 1) && (abs(y - Y) == 1)) || ((abs(x - X) == 1) && (abs(y==Y))) || (((abs(y - Y) == 1)) && (abs(x==X)))) {
+    if (((abs(x - X) == 1) && (abs(y - Y) == 1)) || ((abs(x - X) == 1) && (y==Y)) || (((abs(y - Y) == 1)) && (x==X))) {
         cout << "YES";
         }
         else {
