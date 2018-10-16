62f4e0b472b83284ceecb9f5abaa8980d762983b
YanTszyafen <41192330+YanTszyafen@users.noreply.github.com>
2018-10-17 04:06:28 +0800

    solved 1 tasks

--- src/main/cpp/t05_chess_king.cpp
+++ src/main/cpp/t05_chess_king.cpp
@@ -22,7 +22,7 @@ using namespace std;
 int t05_chess_king() {
     int a, b, c, d;
     cin>>a>>b>>c>>d;
-    if ((((c==a+1) || (c==a-1)) && ((d==b+1) ||(d==b-1))) || ((d == b) || (c == a))) cout<<"YES";
+    if (((c == a+1) || (c == a-1)) && ((d == b+1) || (d == b-1)) || ((d == b) && ((c == a+1) || (c == a-1)) || (c == a) && ((d == b+1) || (d == b-1)))) cout<<"YES";
     else cout<<"NO";
     return 0;
 
