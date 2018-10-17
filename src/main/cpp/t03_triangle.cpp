77a2f7d92c0eb2bd4fe7442bf66c8b7be25898d2
belonel <anatot92>
2018-10-17 12:30:02 +0300

    t03 done

--- src/main/cpp/t03_triangle.cpp
+++ src/main/cpp/t03_triangle.cpp
@@ -20,5 +20,9 @@
 using namespace std;
 
 int t03_triangle() {
-
+    double a, b, c;
+    cin>>a>>b>>c;
+    double p = (a + b + c) / 2;
+    double s = sqrt(p*(p-a)*(p-b)*(p-c));
+    cout<<fixed<<setprecision(4)<<s;
 };
