2b9e592596f708c71b7131b08f5555a1ff68e45e
Vlad Samarin <vlad11-samarin@yandex.ru>
2018-10-23 09:55:43 +0300

    Выполнен task-4

--- src/main/cpp/t03_triangle.cpp
+++ src/main/cpp/t03_triangle.cpp
@@ -20,5 +20,10 @@
 using namespace std;
 
 int t03_triangle() {
+    double s,p,a,b,c;
+    cin>>a >>b >>c;
+    p=(a+b+c)/2;
+    s=(sqrt(p*(p-a)*(p-b)*(p-c)));
+    cout<<fixed<<setprecision(4)<<s;
 
 };
