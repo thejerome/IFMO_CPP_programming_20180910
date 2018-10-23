2b65e6ae7ee3b5831c3c7fff374b6532654f83ea
belonel <anatot92>
2018-10-23 11:02:52 +0300

    t04 and t05 done

--- src/main/cpp/t04_neighbors.cpp
+++ src/main/cpp/t04_neighbors.cpp
@@ -19,5 +19,17 @@
 using namespace std;
 
 int t04_neighbors() {
+    int n;
+    cin>>n;
+    int a[n];
+    for (int i=0; i < n; i++) {
+        cin>>a[i];
+    }
+    for (int i=1; i < n; i++) {
+        if (a[i] * a[i-1] > 0) {
+            cout << a[i - 1] << ' ' << a[i];
+            return 0;
+        }
+    }
 
 }
