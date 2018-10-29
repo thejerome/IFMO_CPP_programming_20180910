05b5a81d244ea0129ea915507ac7d3290bd45bc7
Антон Милорадов <usr.tt@yandex.ru>
2018-10-30 00:00:36 +0300

    t06 (task-4)

--- src/main/cpp/t06_quad.cpp
+++ src/main/cpp/t06_quad.cpp
@@ -17,9 +17,31 @@
 #include "t06_quad.h"
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
+
 int t06_quad() {
+
+    double a, b, c, x1, x2, dis, zero;
+    cout << setprecision(6) << fixed;
+    zero = 1.0e-15;
+    cin >> a >> b >> c;
+    dis = b*b - 4*a*c;
+    if (dis > zero) {
+        // ответ есть
+        x1 = (-b - sqrt(dis)) / (2 * a);
+        x2 = (-b + sqrt(dis)) / (2 * a);
+        if (x1 < x2) cout << x1 << " " << x2;
+        else cout << x2 << " "  << x1;
+    }
+    else if (fabs(dis) <= zero) {
+        if (fabs(b) <=  zero) cout << 0;
+        else {
+            x1 = -b / (2 * a);
+            cout << x1;
+        }
+    };
 
 };
