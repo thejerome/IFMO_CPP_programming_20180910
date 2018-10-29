e8be839d15a41f574a6066594fcb75ec2e4c7e25
Антон Милорадов <usr.tt@yandex.ru>
2018-10-30 00:00:48 +0300

    t07 (task-4)

--- src/main/cpp/t07_quad2.cpp
+++ src/main/cpp/t07_quad2.cpp
@@ -17,9 +17,53 @@
 #include "t07_quad2.h"
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 int t07_quad2() {
+
+    double a, b, c, X1, X2, Discr, Null;
+    cout << setprecision(6) << fixed;
+    Null = 1.0e-7;
+    cin >> a >> b >> c;
+    if (fabs (a) <= Null && fabs (b) <= Null && fabs (c) <= Null)
+    {
+        cout << 3;
+    }
+    else if (not fabs (a) <= Null)
+    {
+        Discr = b * b - 4 * a * c;
+        if (Discr > Null)
+        {
+            cout << 2 << " ";
+            X1 = (-b - sqrt (Discr)) / (2 * a);
+            X2 = (-b + sqrt (Discr)) / (2 * a);
+            if (X1 < X2) cout << X1 << " " << X2;
+            else cout << X2 << " " << X1;
+        }
+        else if (fabs (Discr) <= Null)
+        {
+            cout << 1 << " ";
+            if (fabs (b) <= Null) cout << 0;
+            else
+                {
+                X1 = -b / (2 * a);
+                cout << X1;
+                }
+        }
+        else cout << 0;
+    }
+    else if (fabs (b) <= Null) cout << 0;
+    else
+        {
+        cout << 1 << " ";
+        if (fabs (c) <= Null) cout << 0;
+        else
+            {
+            X1 = -c / b;
+            cout << X1;
+            }
+        }
 
 };
