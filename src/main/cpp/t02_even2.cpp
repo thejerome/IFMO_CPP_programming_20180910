b054a0ec6fa88654467a6fd19c7ff575201a0287
Srezor <srezor.2001@mail.ru>
2018-10-31 16:07:43 +0300

    Task-5 Mrk-1

--- src/main/cpp/t02_even2.cpp
+++ src/main/cpp/t02_even2.cpp
@@ -19,5 +19,13 @@
 using namespace std;
 
 int t02_even2() {
-
+    int n,i;
+    cin >> n;
+    int a[n];
+    for ( i=0; i<n; i++) {
+        cin >> a[i];
+        if (a[i] % 2 == 0)
+            cout << a[i] << " ";
+    }
+        return 0;
 }
--- src/main/cpp/t09_equals.cpp
+++ src/main/cpp/t09_equals.cpp
@@ -26,5 +26,15 @@
 using namespace std;
 
 int t09_equals() {
-
+int n,k=0;
+cin >> n;
+int a[n];
+for (int i=0;i<n; i++)
+    cin >> a[i];
+for (int i=0;i<n-1;i++)
+    for (int j=i+1; j<n; j++)
+        if (a[i]==a[j])
+            k++;
+cout << k;
+return 0;
 }
