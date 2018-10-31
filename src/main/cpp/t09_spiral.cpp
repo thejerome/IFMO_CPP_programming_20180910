fd6f0a94b28680f39e65c0a46c681b1cdd24a378
DimaKurd <d-kurd@yandex.ru>
2018-10-31 11:16:55 +0300

    all sixth task

--- src/main/cpp/t09_spiral.cpp
+++ src/main/cpp/t09_spiral.cpp
@@ -22,6 +22,30 @@
 using namespace std;
 
 int t09_spiral() {
-    
-    return 0;
-}
+    int n, m, a[100][100];
+    cin >> n >> m;
+
+    // fill nearby cells
+    int i = 0, j = -1, count = 0;
+    while (count < n*m) {
+        while (j < m - 1 && !a[i][j+1])
+            a[i][++j] = ++count;    // right
+        while (i < n - 1 && !a[i+1][j])
+            a[++i][j] = ++count;    // down
+        while (j > 0 && !a[i][j-1])
+            a[i][--j] = ++count;    // left
+        while (i > 0 && !a[i-1][j])
+            a[--i][j] = ++count;    // up
+    }
+
+    // output
+    cout.setf(ios::left);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout.width(4);
+            cout << a[i][j];
+        }
+        cout << endl;
+    }
+        return 0;
+    }
