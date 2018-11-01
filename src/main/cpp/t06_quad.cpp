0a194e267eaf8a87137fa9f32547cca5e4bcfae0
Vlad Samarin <vlad11-samarin@yandex.ru>
2018-11-01 12:00:48 +0300

    Выполнен task-4 v 1.1

--- src/main/cpp/t06_quad.cpp
+++ src/main/cpp/t06_quad.cpp
@@ -30,20 +30,13 @@ int t06_quad() {
     d=b*b-4*a*c;
     x1=(-1*b-sqrt(d))/2*a;
     x2=(-1*b+sqrt(d))/2*a;
-    if ((x1!=0)and(x2!=0)){
-        cout <<x1 <<" " <<x2;
-
-
-    }else {
-        if ((x1!=0)and(x2=0)){
-            cout <<x1;
-        }else {
-            if ((x1=0)and(x2!=0)){
-                cout <<x2;
-            }
-
-                }
-            }
+    if (d==0){
+        cout<<(-b/(2*a));
+    }
+    if (d>0){
+        if(d>0)
+            cout<<((-b)-sqrt(d))/(2*a)<<' '<<((-b)+sqrt(d))/(2*a);
+    }
 
 
 
