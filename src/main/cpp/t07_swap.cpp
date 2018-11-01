535668d2b02d05f6618682edc9abc8a7ff89f986
Srezor <srezor.2001@mail.ru>
2018-11-01 09:02:59 +0300

    Fix t07

--- src/main/cpp/t07_swap.cpp
+++ src/main/cpp/t07_swap.cpp
@@ -19,13 +19,13 @@
 using namespace std;
 
 int t07_swap() {
-int n,t;
+int n;
 cin >> n;
 int a[n];
 for (int i=0; i<n; i++)
     cin >> a[i];
 for (int i=1; i<n; i+=2){
-    t=a[i];
+    int t=a[i];
     a[i] = a[i-1];
     a[i-1] = t;
 }
