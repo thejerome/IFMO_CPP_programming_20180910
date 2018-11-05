3e59edc110e698c5290437971aa1b1481361bfaa
Syrazetdinov Max <nicecream66@gmail.com>
2018-11-06 00:07:23 +0300

    Revert "Revert "Done""
    This reverts commit 64d6d913415cce77c374afaa75f8ea3076922bce.

--- src/main/cpp/t01_even.cpp
+++ src/main/cpp/t01_even.cpp
@@ -19,5 +19,13 @@
 using namespace std;
 
 int t01_even() {
+int n;
+cin>>n;
+int A[n];
+    for(int i=0; i<n; i++ ){
+        cin>>A[i];
+        if(i%2==0)
+            cout<<A[i]<<" ";}
+return 0;
 
 }
