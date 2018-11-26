b56f9054279e1c4b76046aa48b0a2feaf8d041bb
belonel <anatot92>
2018-11-26 22:42:52 +0300

    t02 edited, t03 and t04 DONE

--- src/main/cpp/t04_area.cpp
+++ src/main/cpp/t04_area.cpp
@@ -32,9 +32,16 @@
 
 using namespace std;
 
-//function IsPointInArea
+bool IsPointInArea(double x, double y) {
+    return (y >= 2*x+2 && y >= -x && (y-1)*(y-1)+(x+1)*(x+1) <= 4) || (y <= 2*x+2 && y <= -x && (y-1)*(y-1)+(x+1)*(x+1) >= 4);
+}
 
 int t04_area() {
-
+    double x, y;
+    cin>>x>>y;
+    if (IsPointInArea(x, y))
+        cout<<"YES";
+    else
+        cout<<"NO";
     return 0;
 }
