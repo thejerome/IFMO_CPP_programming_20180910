a34acb6703001ae28bea72d01968fb693ffef63f
ogalay <ogalay16@gmail.com>
2018-11-26 20:36:37 +0300

    solved task-7

--- src/main/cpp/t08_queen.cpp
+++ src/main/cpp/t08_queen.cpp
@@ -21,8 +21,30 @@
 #include <vector>
 
 using namespace std;
+int board[10];
+bool check(int i, int j, int k) {
+    if (k == i) return true;
+    else return board[k] != j && (i - k) != (j - board[k]) && (i - k) != (board[k] - j) && check(i, j, k + 1);
+}
+
+int place(int n, int i, int j) {
+    if (i == n) return 1;
+    else {
+        if (j < n) {
+            int r = 0;
+            if (check(i, j, 0)) {
+                board[i] = j;
+                r = place(n, i + 1, 0);
+            }
+            return r + place(n, i, j + 1);
+        }
+        else return 0;
+    }
+}
 
 int t08_queen(){
-
+    int n;
+    cin >> n;
+    cout << place(n, 0, 0);
     return 0;
 }
