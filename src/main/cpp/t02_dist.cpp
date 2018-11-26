312452558ab6db257c6ad2d816305462142a892b
SlavaLapin <vechyaslaugh@gmail.com>
2018-11-26 22:07:22 +0300

    t02 done, test passed

--- src/main/cpp/t02_dist.cpp
+++ src/main/cpp/t02_dist.cpp
@@ -22,8 +22,16 @@
 using namespace std;
 
 //function distance
+double distance(double x1, double y1, double x2, double y2)
+{
+    double result;
+    result = sqrt(pow((x1-x2), 2) + pow((y1-y2), 2));
+    return result;
+}
 
 int t02_dist() {
-
+    double x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    cout << distance(x1, y1, x2, y2);
     return 0;
 }
