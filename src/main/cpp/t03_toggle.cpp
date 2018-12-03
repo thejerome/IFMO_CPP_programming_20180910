985327c59c01d6402b448be8a658e7105f75d686
SlavaLapin <vechyaslaugh@gmail.com>
2018-12-03 21:01:54 +0300

    t03 done, tested

--- src/main/cpp/t03_toggle.cpp
+++ src/main/cpp/t03_toggle.cpp
@@ -9,5 +9,19 @@
 using namespace std;
 
 int t03_toggle() {
-
+    char inp;
+    cin >> inp;
+    if (isalpha(inp))
+    {
+        if(isupper(inp))
+        {
+            inp = (char) tolower(inp);
+        }
+        else
+        {
+            inp = (char) toupper(inp);
+        }
+    }
+    cout << inp;
+    return 0;
 }
