700e4974625e252d9880be64a832084b011048ba
SlavaLapin <vechyaslaugh@gmail.com>
2018-12-03 21:56:33 +0300

    t01 redone without isalpha(), tested

--- src/main/cpp/t01_char.cpp
+++ src/main/cpp/t01_char.cpp
@@ -29,10 +29,22 @@
 
 using namespace std;
 
+bool my_digit_func(char c)
+{
+    char digits[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    for (char digit : digits) {
+        if (digit == c)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int t01_char() {
     char inp;
     cin >> inp;
-    if (isdigit(inp))
+    if (my_digit_func(inp))
     {
         cout<<"yes";
     }
