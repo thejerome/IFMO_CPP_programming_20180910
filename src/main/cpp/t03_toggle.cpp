f6949b1bd4173cbe85d018a9cd77d667f5b503da
SlavaLapin <vechyaslaugh@gmail.com>
2018-12-03 22:32:09 +0300

    t03 redone without isalpha() isupper(), tested

--- src/main/cpp/t03_toggle.cpp
+++ src/main/cpp/t03_toggle.cpp
@@ -5,15 +5,40 @@
 #include "t03_toggle.h"
 #include <iostream>
 
-
 using namespace std;
+
+bool my_alpha_func(char c)
+{
+    auto cc = (int) c;
+    if (((cc >= (int) 'a')&&(cc <= (int) 'z'))||((cc >= (int) 'A')&&(cc <= (int) 'Z')))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool my_upper_func(char c)
+{
+    auto cc = (int) c;
+    if ((cc >= (int) 'A')&&(cc <= (int) 'Z'))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 int t03_toggle() {
     char inp;
     cin >> inp;
-    if (isalpha(inp))
+    if (my_alpha_func(inp))
     {
-        if(isupper(inp))
+        if(my_upper_func(inp))
         {
             inp = (char) tolower(inp);
         }
