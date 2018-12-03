9f645e591790ad3915423dc379573c86a012d09b
Ryzhova Valeria <ryzhova_valieriia@mail.ru>
2018-12-03 10:20:17 +0300

    Done

--- src/main/cpp/t09_equals.cpp
+++ src/main/cpp/t09_equals.cpp
@@ -26,5 +26,18 @@
 using namespace std;
 
 int t09_equals() {
+    int n, k = 0;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < (n - 1); i++) {
+        for (int j = i + 1; j < n ; j++) {
+            if (arr[i] == arr[j])
+                k++;
+        }
+    }
+    cout << k;
 
 }
