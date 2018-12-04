2e844859c5734b3f8b2d7855b8d84aacb810cdfe
Ryzhova Valeria <ryzhova_valieriia@mail.ru>
2018-12-04 14:45:48 +0300

    перезалив задания2
    Signed-off-by: Ryzhova Valeria <ryzhova_valieriia@mail.ru>

--- src/main/cpp/t05_chess_king.cpp
+++ src/main/cpp/t05_chess_king.cpp
@@ -20,5 +20,12 @@
 using namespace std;
 
 int t05_chess_king() {
-
+    int x1, x2, y1, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    if ((x1 - x2 <= 1 && x1 - x2 >= -1) && (y1 - y2 <= 1 && y1 - y2 >= -1)) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
 };
