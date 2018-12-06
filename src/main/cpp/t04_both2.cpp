14b5c1dc44a7f9db25028416a0d15a34c66cb12e
xe-esina <265105@niuitmo.ru>
2018-12-06 18:46:34 +0300

    done task-9

--- src/main/cpp/t04_both2.cpp
+++ src/main/cpp/t04_both2.cpp
@@ -23,9 +23,35 @@
 
 #include "t04_both2.h"
 #include <iostream>
+#include <set>
 
 using namespace std;
 
 int t04_both2() {
+    int n;
+    set<int> s, s1, s2;
 
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        s.insert(x);
+        s1.insert(x);
+    }
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        s.insert(x);
+        s2.insert(x);
+    }
+
+    for (auto it = s.begin(); it != s.end(); ++it)
+        if (s1.count(*it) > 0 && s2.count(*it) > 0)
+            cout << *it << ' ';
 }
--- src/main/cpp/t05_syn.cpp
+++ src/main/cpp/t05_syn.cpp
@@ -27,10 +27,26 @@
 
 #include "t05_syn.h"
 #include <iostream>
-
+#include <map>
+#include <string>
 
 using namespace std;
 
 int t05_syn() {
+    int n;
+    cin >> n;
 
+    map<string, string> m;
+    for (int i = 0; i < n; i++) {
+        string a, b;
+        cin >> a >> b;
+
+        m[a] = b;
+        m[b] = a;
+    }
+
+    string s;
+    cin >> s;
+
+    cout << m[s];
 }
