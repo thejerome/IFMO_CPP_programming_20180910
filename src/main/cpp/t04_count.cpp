0ad8950e82eaf8c331c2906b94e51f7b4dca32e3
Андрей Алексанин <TheTh1rt33nth@protonmail.com>
2018-12-07 13:18:58 +0300

    IT"S WORKING!

--- src/main/cpp/t04_count.cpp
+++ src/main/cpp/t04_count.cpp
@@ -23,10 +23,17 @@
 
 #include "t04_count.h"
 #include <iostream>
-
+#include <string>
 
 using namespace std;
 
 int t04_count() {
-
+	string s;
+	getline(cin, s);
+	int spaces = 0;
+	for (int i = 0; i < s.length(); i++)
+		if (s[i] == ' ')
+			spaces++;
+	cout << spaces + 1;
+	return 0;
 }
