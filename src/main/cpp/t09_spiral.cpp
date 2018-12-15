17d80c67c99c6cb57ff1d7d5b8b5bd2ce2370b93
mpu20 <minhnt1307@gmail.com>
2018-12-15 16:55:10 +0300

    Solved

--- src/main/cpp/t09_spiral.cpp
+++ src/main/cpp/t09_spiral.cpp
@@ -22,6 +22,29 @@
 using namespace std;
 
 int t09_spiral() {
-    
+    int n,m,cnt=1;
+    cin >> n >> m;
+    int a[n][m], x=0, y=0, dx=0, dy=1;
+    for (int i=0; i<n; i++)
+        for (int j=0; j<m; j++) a[i][j]=0;
+    a[x][y]=cnt++;
+    while(true) {
+        bool flag = false;
+        if (x+dx < 0 || x+dx >= n || y+dy < 0 || y+dy >= m || a[x+dx][y+dy] != 0) {
+            if (x+1 < n && a[x+1][y] == 0) dx=1, dy=0;
+            else if (x-1 >= 0 && a[x-1][y] == 0) dx=-1, dy=0;
+            else if (y+1 < m && a[x][y+1] == 0) dx=0, dy=1;
+            else if (y-1 >= 0 && a[x][y-1] == 0) dx=0, dy=-1;
+            else flag = true;
+        }
+        if (flag) break;
+        x += dx;
+        y += dy;
+        a[x][y] = cnt++;
+    }
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) cout << a[i][j] << ' ';
+        cout << endl;
+    }
     return 0;
 }
