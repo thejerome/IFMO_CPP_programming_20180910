823f8ee777b271176197ffc1bb5b0986a9dd41e7
Vikatimoshenko3103 <lalala3103@mail.ru>
2018-12-17 21:54:22 +0300

    Done

--- src/main/cpp/t05_syn.cpp
+++ src/main/cpp/t05_syn.cpp
@@ -1,4 +1,4 @@
-//Вам дан словарь, состоящий из пар слов. 
+﻿//Вам дан словарь, состоящий из пар слов. 
 // Каждое слово является синонимом к парному ему слову. 
 // Все слова в словаре различны. 
 // Для одного данного слова определите его синоним.
@@ -27,10 +27,28 @@
 
 #include "t05_syn.h"
 #include <iostream>
-
+#include <map>
 
 using namespace std;
 
 int t05_syn() {
+ int a;
+    string first_word, second_word;
+    cin >> a;
+    map <string, string> dictionary;
+    for (int i = 0; i < a; i++){
+        cin >> first_word >> second_word;
+        dictionary[first_word] = second_word;
+    }
+    string our_word;
+    cin >> our_word;
+    for (auto now : dictionary){
+        if (now.first == our_word){
+            cout << now.second;
+        }
+        else if (now.second == our_word){
+            cout << now.first;
+        }
+    }
 
 }
