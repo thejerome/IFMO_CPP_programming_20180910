74af12cff2bddce0875af623c473c0775af07151
SlavaLapin <vechyaslaugh@gmail.com>
2018-12-19 15:47:39 +0300

    One last thing to do. No <vector>, no global variables, none of that bullshit I saw in pull requests I previously thought of stealing from. ALL DONE.

--- src/main/cpp/t08_queen.cpp
+++ src/main/cpp/t08_queen.cpp
@@ -18,66 +18,66 @@
 //92
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
-/* int chessboard[10]; // each position in an array represents a column, its value represents a row
 
-bool check_attack(int i, int j, int k)
+bool check_attack(int* board, int queen_number, int n)
 {
-    if (k == i) { return true; }
-    else return chessboard[k] != j 
-                && (i-k) != (j-chessboard[k]) 
-                && (i-k) != (chessboard[k]-j) 
-                && check_attack(i, j, k+1);
-}
 
-int place_queen(int n, int i, int j) {
-    if (i == n) { return 1; }
-        if (j < n) 
+    if (queen_number == 0) return false; // nobody attacks the first queen
+
+    if(queen_number == n-1) {
+        for (int i = queen_number - 1; i > 0; i--) // request a check for attacks for all queens to the left of me
         {
-            int pos = 0;
-            if (check_attack(i, j, 0)) {
-                chessboard[i] = j;
-                pos = place_queen(n, i + 1, 0); // try putting it into each row in this column
-            }
-            return pos + place_queen(n, i, j + 1);
-        } 
-        else return 0;
-}*/
+            if (check_attack(board, i, n)) { return true; }
+        }
+    }
 
-bool check_attack(int* board, int queen_number)
-{
-
-    return
+    for (int i = queen_number-1; i > -1; i--){
+        if (board[i] == board[queen_number]) return true; // horizontal attack
+        if (abs(board[i]-board[queen_number]) == queen_number-i) return true; // diagonal attack
+    }
+    return false;
 }
 
-int add_queen(int* board, int n, int queen_number, int row)
+long add_queen(int* board, int n, int queen_number, int row)
 {
-    long positions = 0;
+    long positions = 0; // possibilities that we need to count
+    int* me = board+queen_number;
+    *me=row;
+
+    for (int i = queen_number-1; i > -1; i--){
+        if (board[i] == board[queen_number]) return 0; // horizontal attack
+        if (abs(board[i]-board[queen_number]) == queen_number-i) return 0; // diagonal attack
+    }
+
 
     if (n == queen_number+1) // leaves on the tree
     {
-        if (check_attack()) { return 1;}
+        if (!check_attack(board, queen_number, n)) { /*cout << "no attacks"<< endl;*/ return 1;}
         else { return 0; }
     }
 
     for (int attempt = 0; attempt < n; attempt++) // try each row...
     {
-        board[queen_number]=row;
         positions+=add_queen(board, n, queen_number+1, attempt); // put queens in next columns
+
     }
 
-
+    return positions;
 }
 
 int t08_queen(){
     int n, *board;
     cin >> n;
     board = (int*) calloc(n, sizeof(int));
-    cout << add_queen(board, n, 0, 0);
-    /*int n;
-    cin >> n;
-    cout << place_queen(n , 0, 0);*/
+    long res = 0;
+
+    for(int i = 0; i < n; i++) // all placements for first queen
+    {
+        res+=add_queen(board, n, 0, i);
+    }
+
+    cout << res;
     return 0;
 }
