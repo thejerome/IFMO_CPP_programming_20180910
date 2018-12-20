1049733352abee4d279c9dfd8d3642dc1648d7c1
Belonel <ksandr.lr@gmail.com>
2018-12-20 21:23:33 +0300

    t01 done

--- src/main/cpp/t01_sort.cpp
+++ src/main/cpp/t01_sort.cpp
@@ -19,10 +19,19 @@
 
 #include "t01_sort.h"
 #include <iostream>
-
+#include <algorithm>
 
 using namespace std;
 
 int t01_sort() {
-    
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        cin>>arr[i];
+    }
+    sort(arr, arr+n);
+    for (int j = 0; j < n; ++j) {
+        cout<<arr[j]<<endl;
+    }
 }
