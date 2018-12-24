819a9d8781aaa442a351fdfa945785b17fb56811
MorozSerge <43163413+MorozSerge@users.noreply.github.com>
2018-12-24 23:36:23 +0300

    P3121 Moroz Sergey task-10

--- src/main/cpp/t03_points.cpp
+++ src/main/cpp/t03_points.cpp
@@ -23,10 +23,32 @@
 
 #include "t03_points.h"
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
+struct point {
+	int x;
+	int y;
+	double sqr_dist;
+};
+
+bool cmp(const point &p1, const point &p2) {
+	return p1.sqr_dist < p2.sqr_dist;
+}
+
 int t03_points() {
-    
+	unsigned int amount_of_points;
+	cin >> amount_of_points;
+	vector <point> point(amount_of_points);
+	for (int i = 0; i < amount_of_points; i++) {
+		cin >> point[i].x >> point[i].y;
+		point[i].sqr_dist = pow(point[i].x, 2) + pow(point[i].y, 2);
+	}
+	sort(point.begin(), point.end(), cmp);
+	for (int i = 0; i < amount_of_points; i++) {
+		cout << point[i].x << " " << point[i].y << endl;
+	}
 }
