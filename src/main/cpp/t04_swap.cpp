89811db0ee9bbadbe7b65c709c5ac682219511c9
SashaOwylde <sasha270900@gmail.com>
2019-03-31 15:33:50 +0500

    .

--- src/main/cpp/t04_swap.cpp
+++ src/main/cpp/t04_swap.cpp
@@ -25,6 +25,24 @@
 using namespace std;
 
 int t04_swap() {
-    
+    int n, m;
+    cin >> n >> m;
+    int a[n][m];
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    int x, y;
+    cin >> x >> y;
+    for (int i=0; i<n; i++) {
+        swap (a[i][x], a[i][y]);
+    }
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cout << a[i][j] << ' ';
+        }
+        cout << endl;
+    }
     return 0;
 }
